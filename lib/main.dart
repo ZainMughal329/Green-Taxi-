e60fe2e591af4b4e7acfa57c0c29690c503fc296
ZainMughal329 <zainmughal4379329@gmail.com>
2023-08-09 15:08:51 +0500

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Text("Project Created"),
+          ),
+        ),
+      ),
+    );
+  }
+}
